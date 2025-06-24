2c8de41306b8453066bf27878325d3f61e9264e7
Er.Abhishek Mishra <abhishekmishra59@gmail.com>
2025-06-24 18:08:52 +0530

    add origin site

--- routes/student.js
+++ routes/student.js
@@ -37,8 +37,8 @@ router.post('/register', async (req, res) => {
     // 4. Set cookie with token
     res.cookie('token', token, {
       httpOnly: true,       // prevents client-side JS access
-      secure: false,        // set to true in production with HTTPS
-      sameSite: 'strict',   // CSRF protection
+      secure: true,        // set to true in production with HTTPS
+      sameSite: 'none',     // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
@@ -73,8 +73,8 @@ try {
                 const token = generateCookies(user._id);
                   res.cookie('token', token, {
       httpOnly: true,       // prevents client-side JS access
-      secure: false,        // set to true in production with HTTPS
-      sameSite: 'strict',   // CSRF protection
+       secure: true,        // set to true in production with HTTPS
+      sameSite: 'none',      // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
             
